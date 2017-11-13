187a76815156e91739333742cb1e8224496a84f3
MIKI Soichiro <miki.soichiro@gmail.com>
2017-11-13 19:55:37 +0900

    First commit for DM

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,10 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     get :timeline, on: :collection
   end
-  
+  resources :conversations do
+    resources :messages
+  end
+
   root to: 'registrations#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
